2f67b62c163519a0dfdae81cc9733a0e2552f167
Fadil16 <92834833+Fadil16@users.noreply.github.com>
2024-06-13 23:07:37 +0700

    Add files via upload

---
+++ Pertemuan2.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title> Biodata </title>
+    </head>
+    <body>
+        <h1 align="center">My Curriculum Vitae</h1>
+
+        <?php
+
+        echo 'Biodata diri <br>';
+        echo '<br>';
+        echo '<hr>';
+        $nama="Aditya Rafadilla";
+        $alamat="Bojong Gede";
+        $email="adityarafa@gmail.com";
+        $pekerjaan="Mahasiswa";
+        $hobi="Basket";
+        echo $nama;
+        echo "<br>";
+        echo $alamat;
+        echo "<br>";
+        echo $email;
+        echo "<br>";
+        echo $pekerjaan;
+        echo "<br>";
+        echo $hobi;
+
+
+
+
+        ?>
+    </body>
